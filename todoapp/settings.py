b66a68146fb78ea763aa1cbf6f34701e0540d4c2
tinchoz8426 <76183907+tinchoz8426@users.noreply.github.com>
2022-07-15 16:16:17 -0300

    de nuevo

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -83,7 +83,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.pg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
